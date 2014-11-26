0f62bb8220f2a3e0f59b9683a4cf2d44ae9e3bc4
leviross <levross@gmail.com>
2014-11-25 21:56:43 -0800

    really fun and learned a ton

--- Car.js
+++ Car.js
@@ -1,20 +1,80 @@
-function Car(make, model, year, color, seats){
+function Car(make, model, year, color, seats, passengers){
   this.make = make;
   this.model = model;
   //TODO: add year, color, seats here
+  this.year = year;
+  this.color = color; 
+  this.seats = seats; 
 
   this.running = false;
   this.owner = "manufacturer";
   this.previousOwners = [];
+  this.passengers = passengers || [];
 }
 
 Car.prototype.sell = function(newOwner){
-
+	this.previousOwners.push(this.owner);
+	this.owner = newOwner;
 };
 
 Car.prototype.paint = function(newColor){
-
+	this.color = newColor; 
 };
+
+Car.prototype.start = function(){
+	this.running = true; 
+}
+Car.prototype.off = function() {
+	this.running = false;
+}
+Car.prototype.driveTo = function(destination) {
+	if (this.running === true){
+	console.log("Driving to " + destination)
+	return true; 
+	}
+	return false;
+	}
+Car.prototype.park = function() {
+	if (this.running === false) {
+		console.log("parked!!!")
+		return true; 
+	}
+	return false; 
+}	
+Car.prototype.pickUp = function(name) {
+	if ((this.running) && (this.seats > this.passengers.length+1)) {
+		console.log("driving to pickup " + name);
+		this.passengers.push(name);
+		return true;
+	}
+	return false; 
+}
+
+Car.prototype.dropOff = function(name) {
+	var rider = this.passengers.indexOf(name);
+	if (rider >= 0 && this.running === true) {
+		this.passengers.splice(rider, 1);
+		console.log("driving to drop off " + name);
+		return true; 
+	}
+		return false;
+}
+
+Car.prototype.passengerCount = function() {
+	return (this.passengers.length); 
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
